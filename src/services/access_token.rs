a96c2e6971062eb64396803a23fbc5faeba059f5
scrom <formain337@gmail.com>
2026-01-07 15:18:50 +0000

    add: get_access_token fn for the end user and also now the filepath stays in the struct as it is needed in the future to generate more tokens

--- src/services/access_token.rs
+++ src/services/access_token.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
@@ -10,10 +10,11 @@ use crate::{errors::DriveDbError, services::jwt_builder::JwtBuilder};
 struct AccessToken {
     access_token: String,
     expiry_time: u64,
+    file_path: PathBuf,
 }
 
 impl AccessToken {
-    async fn get_access_token(file_path: &Path) -> Result<(String, Self), DriveDbError> {
+    async fn get_access_token_self(file_path: &Path) -> Result<Self, DriveDbError> {
         let (signed_jwt, expiry_time) = JwtBuilder::new(file_path)?;
         let client = Client::new();
         let token_resp: Value = client
@@ -29,12 +30,20 @@ impl AccessToken {
 
         let access_token = token_resp["access_token"].to_string();
 
-        Ok((
-            access_token.clone(),
-            Self {
-                access_token,
-                expiry_time,
-            },
-        ))
+        Ok(Self {
+            access_token,
+            expiry_time,
+            file_path: file_path.to_path_buf(),
+        })
+    }
+
+    async fn get_access_token(&mut self) -> Result<String, DriveDbError> {
+        let (current_epoch_time, _) = JwtBuilder::get_epoch_time();
+
+        if current_epoch_time > self.expiry_time {
+            *self = Self::get_access_token_self(&self.file_path).await?;
+        };
+
+        Ok(self.access_token.clone())
     }
 }
